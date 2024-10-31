defb8ee8d897860d8e92a8ec078765ab3ad5e219
JMoonblatt <moonbl62@students.rowan.edu>
2024-10-31 19:25:29 -0400

    Update query.js

--- query.js
+++ query.js
@@ -1,6 +1,12 @@
-function findUser(change, collection) {
-    const locations = change.fullDocument.origins.push(change.fullDocument.destination);
-    console.log(locations[1]);
+function findUser(change) {
+    // Check if fullDocument and the necessary fields exist before accessing them
+    if (change.fullDocument && change.fullDocument.origins && change.fullDocument.destination) {
+        const locations = change.fullDocument.origins.push(change.fullDocument.destination);
+        return locations;
+    } else {
+        console.error("Document structure is unexpected:", change.fullDocument);
+        return null;
+    }
 }
 
-module.exports = { findUser }
+module.exports = { findUser };
